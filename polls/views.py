ae73b8c66ffe97be96cd6abbd8e1d74a3b6c5155
developer <ddd98754@gmail.com>
2023-02-15 16:19:40 +0900

    投票機能と結果画面実装

--- polls/views.py
+++ polls/views.py
@@ -19,17 +19,17 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
-def vote(result, question_id):
+def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
         seleced_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
         return render(request, 'polls/detail.html', {
             'question':question,
-            'error_message': "You're didn't select a choice."
+            'error_message': "You didn't select a choice."
         })
     else:
         seleced_choice.votes += 1
